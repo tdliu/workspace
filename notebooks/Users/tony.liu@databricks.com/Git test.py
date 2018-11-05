f6dd6c5c47a253ddb7f8663c5c34859cd03d8206
tdliu <tliu.auckland@gmail.com>
2018-11-05 13:48:52 -0800

    first commit

---
+++ notebooks/Users/tony.liu@databricks.com/Git test.py
@@ -0,0 +1,22 @@
+# Databricks notebook source
+1+1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Test
+
+# COMMAND ----------
+
+a = 23
+
+# COMMAND ----------
+
+b = a*23
+
+# COMMAND ----------
+
+print(b)
+
+# COMMAND ----------
+
