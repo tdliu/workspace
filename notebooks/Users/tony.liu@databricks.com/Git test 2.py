eed05ddabbd8b8edbaf6701d9c03b1a25f83331c
tdliu <tliu.auckland@gmail.com>
2018-11-08 16:11:37 -0800

    Add files via upload

---
+++ notebooks/Users/tony.liu@databricks.com/Git test 2.py
@@ -0,0 +1,30 @@
+# Databricks notebook source
+1+1
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Test
+
+# COMMAND ----------
+
+a = 23
+
+# COMMAND ----------
+
+b = a*23
+
+# COMMAND ----------
+
+print(b)
+
+# COMMAND ----------
+
+
+
+# COMMAND ----------
+
+print("This is the autosave branch test")
+
+# COMMAND ----------
+
